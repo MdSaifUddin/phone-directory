af86bb89b139524db871a9eb1660f9615ec5f011
Saif <uddinsaif81@gmai.com>
2019-07-25 03:10:24 +0530

    inline styling

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Header=function(){
+    return(
+        <div style={{textAlign:'center',padding:20,textTransform:'uppercase',background:'#000',color:'white'}}>
+        Phone Directory
+        </div>
+    )
+    }
+export default Header;
